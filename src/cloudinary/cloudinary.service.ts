325b065bb9bf69d4559ee102935a153f858b7db3
Erwin <memon_16@hotmail.com>
2026-06-20 20:11:48 -0400

    Move cloudinary config to method call to fix env loading

--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -4,6 +4,10 @@ import { v2 as cloudinary } from 'cloudinary';
 @Injectable()
 export class CloudinaryService {
   async uploadImage(fileBuffer: Buffer, bookingId: string): Promise<string> {
+    console.log('CLOUD_NAME:', process.env.CLOUDINARY_CLOUD_NAME);
+    console.log('API_KEY:', process.env.CLOUDINARY_API_KEY);
+    console.log('API_SECRET exists:', !!process.env.CLOUDINARY_API_SECRET);
+
     cloudinary.config({
       cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
       api_key: process.env.CLOUDINARY_API_KEY,
